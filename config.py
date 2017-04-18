eece857a6cf5740b826fa307f54151d86d68d2ed
YanB25 <442826556@qq.con>
2017-04-18 20:51:18 +0800

    update

--- config.py
+++ config.py
@@ -2,3 +2,11 @@ from math import *
 unit =  3/4
 confidence = 0.99
 TEST_TIMES = ceil(log(1 - confidence, 2) / log(unit, 2))
+
+
+bmin = 2**511
+bmax = 2**512 - 1
+
+wid_min = log(bmin, 10)
+wid_max = log(bmin, 10)
+
